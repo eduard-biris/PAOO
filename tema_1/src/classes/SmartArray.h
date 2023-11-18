8b99eb24c88fd39be90599dae31e9aa9f68e64c8
unknown <eduardandreibiris@yahoo.com>
2023-11-18 23:34:53 +0200

    Added directory for tema-1

---
+++ tema_1/src/classes/SmartArray.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <vector>
+
+class SmartArray {
+    private:
+        int n = 0;
+        int avMemory = 0;
+        int *array = NULL;
+    public:
+        SmartArray(int totalSize);
+        SmartArray(const SmartArray& arrayToCopy);
+        SmartArray(SmartArray&& arrayToMove);
+        SmartArray(std::vector<int> initialData = {});
+        ~SmartArray();
+        void push(int x);
+        void pop();
+        void sortArray();
+        void print();
+};
